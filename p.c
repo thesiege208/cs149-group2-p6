c6d2634e2a3976c844532860d025f0cdd05cc98b
Tianyun <451617222@qq.com>
2018-11-29 09:51:36 -0800

    new verison

--- p.c
+++ p.c
@@ -3,113 +3,132 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/time.h>
-#include <sys/ioctl.h>
 
-#define BUFFER_SIZE 32
+#define BUFFER_SIZE 128
 #define READ_END     0
 #define WRITE_END    1
 
+int64_t getCurrentTime() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
 int main(void) {
-    char write_msg[BUFFER_SIZE] = "Child process!";
+    char write_msg[BUFFER_SIZE];
     char read_msg[BUFFER_SIZE];
-    char buffer[128];
-
-    pid_t pid;  // child process id
-    int fd[2];  // file descriptors for the pipe
-
+    char buf[BUFFER_SIZE];    // used for writing of child5
+    
+    int fd[2], i;     // file descriptors for the pipe
+    pid_t pid;        // child process id
+    
+    int64_t start = getCurrentTime();
+    int64_t finish;
+    double sT;
+    double spendTime[20];
+    
+    FILE *fp;
+    fp = fopen("output.txt", "w");
+    
     // Create the pipe.
     if (pipe(fd) == -1) {
         fprintf(stderr, "pipe() failed");
-        return 1;
+        exit(1);
     }
-
+    
+    
     // Fork five child process.
-    //pid = fork();
-    for (int i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++) {
+        int random = rand() % 3;
+        sleep(rand() % 3);
+        finish = getCurrentTime();
+        spendTime[i] = difftime(finish, start) / 1000;
         pid = fork();
         // random 0 1 2s
-        sleep(rand()%3);
-        if (pid == 0 || pid == -1) {
-            printf("create child process id=%d, parent process id=%d\n", getpid(), getppid());
-
+        if (pid == 0) {
+            
+            
+            printf("Time: %f , child %d, create childId=%d, parentId=%d\n", spendTime[i], i + 1, getpid(), getppid());
+            if (fp != NULL)
+                fprintf(fp, "Time: %f , child %d, create childId=%d, parentId=%d\n", spendTime[i], i + 1, getpid(),
+                        getppid());
             break;
-
+        } else if (pid == -1) {
+            perror("pipe error");
+            exit(1);
         }
     }
-
-    if (pid > 0) {
-        int result, nread;
-        fd_set inputs, inputfds; // sets of file descriptors
-        struct timeval timeout;
-
-        // PARENT PROCESS.
-        //printf("Parent process\n");
-        // Close the unused READ end of the pipe.
-        close(fd[WRITE_END]);
-        read(fd[READ_END], read_msg, BUFFER_SIZE);
-        printf("Parent: Read '%s' to the pipe. process id=%d\n", read_msg, getpid());
-
-        FD_ZERO(&inputs);       // initialize inputs to the empty set
-        FD_SET(0, &inputs);     // set file descriptor read_msg (stdin)
-        timeout.tv_sec = 2;
-        timeout.tv_usec = 500000;
-
-        for (;;) {
-            //inputfds = read_msg;
-            // Write to the WRITE end of the pipe.
-            inputfds=inputs;
-            result = select(FD_SETSIZE, &inputfds, (fd_set *) 0, (fd_set *) 0, &timeout);
-
-
-            if (result == -1) {                // timeout w/o input
-                printf("@");
-                fflush(stdout);
-            } else if (result == 0) {// error
-                printf("select");
-                exit(1);
-            } else { // Got input
-                printf("else");
-                // If, during the wait, we have some action on the file descriptor,
-                // we read the input on stdin and echo it whenever an
-                // <end of line> character is received, until that input is Ctrl-D.
-
-                if (FD_ISSET(0, &inputfds)) {
-                    ioctl(0, FIONREAD, &nread);
-                    if (nread == 0) {
-                        printf("Keyboard input done.\n");
-                        exit(0);
-                    }
-
-                    nread = read(0, buffer, nread);
-                    buffer[nread] = 0;
-                    printf("Read %d characters from the keyboard: %s",
-                           nread, buffer);
-                }
-
-            }
-            // Close the WRITE end of the pipe.
-            //close(fd[READ_END]);
-            //exit(1);
-        }
-    } else if (pid == 0) {
+    
+    if (i == 0) {
         // CHILD PROCESS.
         //printf("Child process\n");
         // Close the unused WRITE end of the pipe.
         close(fd[READ_END]);
-
+        
         // Read from the READ end of the pipe.
-        write(fd[WRITE_END], write_msg, strlen(write_msg) + 1);
-        printf("Child : Wrote '%s' from the pipe. process id=%d\n", write_msg, getpid());
+        write(fd[WRITE_END], "child1", BUFFER_SIZE);
         // Close the READ end of the pipe.
         close(fd[WRITE_END]);
+        sleep(30);
         exit(1);
+    } else if (i == 1) {
+        // CHILD PROCESS.
+        close(fd[READ_END]);
+        write(fd[WRITE_END], "child2", BUFFER_SIZE);
+        close(fd[WRITE_END]);
+        sleep(30);
+        exit(1);
+    } else if (i == 2) {
+        // CHILD PROCESS.
+        close(fd[READ_END]);
+        write(fd[WRITE_END], "child3", BUFFER_SIZE);
+        close(fd[WRITE_END]);
+        sleep(30);
+        exit(1);
+    } else if (i == 3) {
+        // CHILD PROCESS.
+        close(fd[READ_END]);
+        write(fd[WRITE_END], "child4", BUFFER_SIZE);
+        close(fd[WRITE_END]);
+        sleep(30);
+        exit(1);
+    } else if (i == 4) {
+        // CHILD PROCESS.
+        close(fd[READ_END]);
+        for (int x = 0; x < 5; x++) {
+            finish = getCurrentTime();
+            spendTime[5+x] =difftime(finish, start) / 1000;
+            memset(buf, 0, sizeof(buf));
+            printf("child5, Please input: \n");
+            scanf("%s", buf);
+            printf("\n");
+            write(fd[WRITE_END], buf, BUFFER_SIZE);
+        }
+        close(fd[WRITE_END]);
+        sleep(30);
+        exit(1);
+        
     } else {
-        fprintf(stderr, "fork() failed");
-        return 1;
+        // PARENT PROCESS.
+        close(fd[WRITE_END]);
+        // Close the unused READ end of the pipe.
+        for (i = 0; i < 9; i++) {
+            finish = getCurrentTime();
+            
+            memset(read_msg, 0, sizeof(read_msg));
+            read(fd[READ_END], read_msg, BUFFER_SIZE);
+            
+            printf("ParentTime=%f, Read '%s' to the pipe, id=%d, childTime=%f\n", difftime(finish, start) / 1000,
+                   read_msg, getpid(), spendTime[i]);
+            fprintf(fp, "ParentTime=%f, Read '%s' to the pipe, id=%d, childTime=%f\n", difftime(finish, start) / 1000,
+                    read_msg, getpid(), spendTime[i]);
+            //wait(NULL);
+        }
+        sleep(30);
+        exit(0);
     }
-
+    
     return 0;
 }
 
